3f041185c7088c8c0bdd72d93a8faadd3ee61a84
Bryan Arius <bryanarius@gmail.com>
2026-07-26 18:43:40 -0400

    Create Footer

---
+++ app/Views/layouts/footer.php
@@ -0,0 +1,12 @@
+<footer class="site-footer">
+    <div class="container">
+        <p>
+            Brymon Team Builder &copy;
+            <?= date('Y') ?> Bryan Arius
+        </p>
+
+        <p>Built with PHP, PostgreSQL, and PokéAPI.</p>
+    </div>
+</footer>
+</body>
+</html>
