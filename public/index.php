94597c30a58a5e55b09575a08815ea0e1c269cf9
Bryan Arius <bryanarius@gmail.com>
2026-07-26 18:33:12 -0400

    Create Front Controller

--- public/index.php
+++ public/index.php
@@ -1,1 +1,16 @@
+<?php
 
+declare(strict_types=1);
+
+use App\Core\Router;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$router = new Router();
+
+require dirname(__DIR__) . '/routes/web.php';
+
+$router->dispatch(
+    $_SERVER['REQUEST_METHOD'] ?? 'GET',
+    $_SERVER['REQUEST_URI'] ?? '/'
+);
--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,6 @@
 declare(strict_types=1);
 
 use App\Controllers\PageController;
-use App\Controllers\PokemonController;
-use App\Controllers\TeamController;
-
-/*
-|--------------------------------------------------------------------------
-| Route Parameters
-|--------------------------------------------------------------------------
-|
-| {id}   - Team ID
-| {name} - Pokémon name
-|
-*/
 
 /*
 |--------------------------------------------------------------------------
@@ -25,44 +13,3 @@ use App\Controllers\TeamController;
 $router->get('/', [PageController::class, 'home']);
 
 $router->get('/about', [PageController::class, 'about']);
-
-
-/*
-|--------------------------------------------------------------------------
-| Team Routes
-|--------------------------------------------------------------------------
-*/
-
-// Display all saved teams
-$router->get('/teams', [TeamController::class, 'index']);
-
-// Display the create team page
-$router->get('/teams/create', [TeamController::class, 'create']);
-
-// Save a new team
-$router->post('/teams', [TeamController::class, 'store']);
-
-// Display a single team
-$router->get('/teams/{id}', [TeamController::class, 'view']);
-
-// Display the edit team page
-$router->get('/teams/{id}/edit', [TeamController::class, 'edit']);
-
-// Update an existing team
-$router->post('/teams/{id}/update', [TeamController::class, 'update']);
-
-// Delete a team
-$router->post('/teams/{id}/delete', [TeamController::class, 'destroy']);
-
-
-/*
-|--------------------------------------------------------------------------
-| Pokémon Routes
-|--------------------------------------------------------------------------
-*/
-
-// Search for Pokémon
-$router->get('/pokemon', [PokemonController::class, 'search']);
-
-// Display information for a specific Pokémon
-$router->get('/pokemon/{name}', [PokemonController::class, 'show']);
